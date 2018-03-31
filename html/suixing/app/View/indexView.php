ee160e40af97f3fca448d857fcb6f5922bcf775b
zzw <2714450332@qq.com>
2018-03-31 18:34:26 +0800

    1.3.10

--- html/suixing/app/View/indexView.php
+++
@@ -1,9 +0,0 @@
-<?php
-    namespace app\View;
-    class index{
-
-        function display($file,$data){
-            \lib\core\view::display($file,$data);
-        }
-    }
-?>
--- html/suixing/app/View/trainsView.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace app\View;
-class trains{
-		function show_trains($datas){
-			\lib\core\view::display('details',$datas);
-		}
-}
-?>
