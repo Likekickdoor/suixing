379a8be51c5819d76d95e567c488087146afa7b5
zzw <2714450332@qq.com>
2018-04-23 13:32:53 +0800

    1.4.3
    修复了推荐栏的bug和转乘查不到的bug

--- app/Model/centerCityModel.php
+++ app/Model/centerCityModel.php
@@ -149,39 +149,6 @@
             $this->end = $_GET['end'];
         }
 
-        private function get_data($tool){
-            $result = DB::findAll("center_city","{$tool} like '%{$this->end}%'");
-            // var_dump($result);die;
-            if(!empty($result)){
-                foreach ($result as $value) {
-                    // p($value['place']);
-                    if(strpos($this->bus_center_city,$value['place']) != false)
-                        $bus[] = $value['place'];
-                    if(strpos($this->train_center_city,$value['place']) != false)
-                        $train[] = $value['place'];
-                    if(strpos($this->flight_center_city,$value['place']) != false)
-                        $flight[] = $value['place'];
-                    if(strpos($this->ship_center_city,$value['place']) != false)
-                        $ship[] = $value['place'];
-                }
-                if(!empty($bus))
-                    $citys[] = $bus;
-                else $citys[] = NULL;
-                if(!empty($train))
-                    $citys[] = $train;
-                else $citys[] = NULL;
-                if(!empty($flight))
-                    $citys[] = $flight;
-                else $citys[] = NULL;
-                if(!empty($ship))
-                    $citys[] = $ship;
-                else $citys[] = NULL;
-                // if(!empty($citys[0]) || !empty($citys[1]) || !empty($citys[2]) || !empty($citys[3]))
-                    $this->data[] = $citys;
-                // var_dump($tool);p("");
-            }
-        }
-
         function getCenterCity(){
             $this->init();
             $bus = [];
@@ -191,18 +158,16 @@
             $result = DB::find("center_city","place like '%{$this->start}%'");
             // var_dump($result);die;
             if($result != NULL){
-                $this->bus_center_city = $result['bus'];
-                $this->train_center_city = $result['train'];
-                $this->flight_center_city = $result['flight'];
-                $this->ship_center_city = $result['ship'];
-                // $all_center_city = $this->bus_center_city.$this->train_center_city.$this->flight_center_city.$this->ship_center_city;
-                // var_dump($all_center_city);die;
-                $this->get_data('bus');
-                $this->get_data('train');
-                $this->get_data('flight');
-                $this->get_data('ship');
-                // var_dump($this->data);die;
-                return $this->data;
+                $place = DB::findAll("center_city","bus like '%{$this->end}%' or train like '%{$this->end}%' or flight like '%{$this->end}%' or ship like '%{$this->end}%'","place");
+                $all_places = "a";
+                $all_places .= $result['bus'].$result['train'].$result['flight'].$result['ship'];
+                // var_dump($all_places);die;
+                foreach ($place as $value) {
+                    // var_dump($value);
+                    if(strpos($all_places,$value['place']) != false)
+                        $citys[] = $value['place'];
+                }
+                return $citys;
             }else{
                 $station_id = DB::find("station","name='{$this->start}'")['id'];
                 // p($station_id);die;
